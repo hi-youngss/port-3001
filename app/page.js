7f7a4afadb1daabf34eb0a68a184a744d7d47ea2
young <hi-youngss>
2025-01-24 15:39:18 +0900

    빌드 실패 테스트

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
+  useEffect(() => {}, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h3>3001포트!!!</h3>
+        <h3>3001포트!!!다시!!!</h3>
+
         <Image
           className="dark:invert"
           src="/next.svg"
